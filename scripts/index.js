9bb98443e086aa052deb2be4954ec4dd0eaf2076
Pat Pham <patpham505@gmail.com>
2024-12-01 19:09:03 -0800

    created js file

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+// Array to store initial cards
+const initialCards = [
+    {
+      name: "Yosemite Valley",
+      link: "https://images.unsplash.com/photo-1585366119953-f903c748d620?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=1080"
+    },
+    {
+      name: "Lake Louise",
+      link: "https://images.unsplash.com/photo-1559767940-52eb7a827f6b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=1080"
+    },
+    {
+      name: "Bald Mountain",
+      link: "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=1080"
+    },
+    {
+      name: "Grand Canyon",
+      link: "https://images.unsplash.com/photo-1568605117036-5fe5e7bab0b7?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=1080"
+    },
+    {
+      name: "Rocky Mountains",
+      link: "https://images.unsplash.com/photo-1522176027015-21d3b58b6891?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=1080"
+    },
+    {
+      name: "Alberta Prairie",
+      link: "https://images.unsplash.com/photo-1518110618905-c1d4457512c6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=1080"
+    }
+  ];
+  
+  // Output initial cards to the console (for testing purposes)
+  console.log(initialCards);
+  
